bc31091e5ab3fa4b0be0d6e5f050f8f088498e53
Casey Dunn <casey.dunn@yale.edu>
2020-05-16 21:46:47 -0400

    works, but connection breaks

---
+++ nrf24cw/nrf24cw.ino
@@ -0,0 +1,81 @@
+#include <SPI.h>
+#include "RF24.h"
+
+int button_pin = 2;
+int led_pin = 3;
+int id_1 = 4;
+int id_2 = 5;
+int id_4 = 6;
+
+// Building on https://tmrh20.github.io/RF24/GettingStarted_8ino-example.html
+
+int radioNumber = 0;
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);  // CE, CSN
+
+const byte addresses [][6] = {"00001", "00002"};  //Setting the two addresses. One for transmitting and one for receiving
+
+boolean button_state = 0;
+boolean remote_state = 0;
+
+void setup() {
+  pinMode( button_pin, INPUT_PULLUP );
+  attachInterrupt(digitalPinToInterrupt(button_pin), key_change, CHANGE);
+  pinMode( led_pin, OUTPUT );
+  pinMode( id_1, INPUT_PULLUP );
+  pinMode( id_2, INPUT_PULLUP );
+  pinMode( id_4, INPUT_PULLUP );
+
+  Serial.begin(9600);
+  Serial.println(F("RF24 CW"));
+
+  radioNumber = 
+    1 * digitalRead( id_1 ) + 
+    2 * digitalRead( id_2 ) + 
+    4 * digitalRead( id_4 ) ;
+
+  Serial.print( "Radio number: " );
+  Serial.println( radioNumber );
+  
+  radio.begin();
+
+  // Set up the pipes. For now, assume two radios with bidirectional communication
+  if ( radioNumber == 0 ){
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1, addresses[1]);
+  }
+  else{
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1, addresses[0]);    
+  }
+  
+  radio.setPALevel( RF24_PA_LOW );
+  
+
+}
+
+// Monitor for traffic
+void loop() {
+  delay(5);
+  radio.startListening();
+  if ( radio.available()) {
+    radio.read(&remote_state, sizeof(remote_state));
+    if ( remote_state ){
+      digitalWrite(led_pin, HIGH);
+    }
+    else{
+      digitalWrite(led_pin, LOW);
+    }
+  }
+}
+
+// Called whenever the local button changes
+// Based on https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
+void key_change() {
+    radio.stopListening();
+    button_state = ! digitalRead( button_pin );
+    radio.write(&button_state, sizeof(button_state));
+
+    Serial.print( "Local button: " );
+    Serial.println( button_state );
+}
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Arduino wireless
+
+
+## NRF24
+
+### Hardware
+
+Pinout - https://tmrh20.github.io/RF24/
+
+Purchased these:
+
+- https://www.amazon.com/gp/product/B06WLH4ZG6/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1
+
+- https://www.amazon.com/gp/product/B00O9O868G/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1
+
+- https://www.ebay.com/itm/NRF24L01-PA-LNA-SMA-Antenna-Wireless-Transceiver-communication-module-2-4G-1100m/310651702557?ssPageName=STRK%3AMEBIDX%3AIT&_trksid=p2057872.m2749.l2649
+
+
+### Tutorials
+
+#### Electronoob
+
+https://www.youtube.com/watch?v=vxF1N9asjts
+
+http://electronoobs.com/eng_arduino_tut95.php
+
+#### Mechatronics
+
+Bidirectional communication -
+- https://www.youtube.com/watch?v=7rcVeFFHcFM
+
+- https://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
+
+### Libraries
+
+#### RF24
+
+https://tmrh20.github.io/RF24/
+
+### My project
+
+D2 - button
+D3 - LED
+D4 - ID1
+D5 - ID2
+D6 - ID3
+D7 - CE  NRF24
+D8 - CSN NRF24
+
+Using the RF24 library
